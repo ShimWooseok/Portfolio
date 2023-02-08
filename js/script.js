83d193138bda2412b867e5fdd950f370241953c2
swss <zaqplm7913@naver.com>
2023-02-08 12:21:47 +0900

    23.02.08

--- js/script.js
+++ js/script.js
@@ -10,6 +10,90 @@ $(document).ready(function(){
 
 
   })
+  $(".circle01").circleProgress({
+    // 그래프 시작 위치
+    // Math.PI = 3.14
+    startAngle: -Math.PI/2,
+    // 방향 반전 여부
+    reverse:true,
+    // 그래프 수치
+    value: 0.75,
+    // 그래프 사이즈
+    size: 220,
+    // 그래프 모서리, 지울 시 각진 모양
+
+    // 그래프 두께
+    thickness: 30,
+    // 그래프 색상
+    fill: {
+      gradient: ["#B9F3FC", "#7EBEE5"]
+    }
+  })
+  $(".circle02").circleProgress({
+    // 그래프 시작 위치
+    // Math.PI = 3.14
+    startAngle: -Math.PI/2,
+    // 방향 반전 여부
+    reverse:true,
+    // 그래프 수치
+    value: 0.75,
+    // 그래프 사이즈
+    size: 220,
+    // 그래프 모서리, 지울 시 각진 모양
+
+    // 그래프 두께
+    thickness: 30,
+    // 그래프 색상
+    fill: {
+      gradient: ["#B9F3FC", "#7EBEE5"]
+    }
+  })
+  $(".circle03").circleProgress({
+    // 그래프 시작 위치
+    // Math.PI = 3.14
+    startAngle: -Math.PI/2,
+    // 방향 반전 여부
+    reverse:true,
+    // 그래프 수치
+    value: 0.9,
+    // 그래프 사이즈
+    size: 220,
+    // 그래프 모서리, 지울 시 각진 모양
+
+    // 그래프 두께
+    thickness: 30,
+    // 그래프 색상
+    fill: {
+      gradient: ["#B9F3FC", "#7EBEE5"]
+    }
+  })
+  $(".circle04").circleProgress({
+    // 그래프 시작 위치
+    // Math.PI = 3.14
+    startAngle: -Math.PI/2,
+    // 방향 반전 여부
+    reverse:true,
+    // 그래프 수치
+    value: 0.9,
+    // 그래프 사이즈
+    size: 220,
+    // 그래프 모서리, 지울 시 각진 모양
+
+    // 그래프 두께
+    thickness: 30,
+    // 그래프 색상
+    fill: {
+      gradient: ["#B9F3FC", "#7EBEE5"]
+    }
+  })
+
+  // 그래프 수치 출력
+  .on("circle-animation-progress", function (event, progress, stepValue) {
+    $(this)
+      .find(".percent")
+      .text(parseInt(stepValue * 100) + "%");
+  });
+
 
 })
 $(window).scroll(function () {
